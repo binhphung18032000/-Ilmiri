4f178a1315cb7deb322a616468f333d889889d55
Binh Phung <binhphung18032000@gmail.com>
2023-10-25 21:54:08 +0700

    Update worked before

--- wp-content/themes/hello-elementor-child/header.php
+++ wp-content/themes/hello-elementor-child/header.php
@@ -30,33 +30,51 @@ if (!defined('ABSPATH')) {
     <header class="m-header-main">
         <div class="container">
             <a href="/" class="m-logo-mobile">
+                <?php
+                if (function_exists('the_custom_logo')) {
+                    $custom_logo_id = get_theme_mod('custom_logo');
+                    $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+                }
+                ?>
+                <img src="<?php echo $logo[0]; ?>" alt="Logo" />
+            </a>
+			<input type="checkbox" id="check__toggle" hidden />
+            <label for="check__toggle" class="btn__toggle"><i class="fa-solid fa-bars"></i></label>
+            <nav role="navigation" aria-label="Main Menu" class="m-nav">
+				<a href="/" class="m-logo-mobile" id="m-logo-mobile-active">
                     <?php
                     if (function_exists('the_custom_logo')) {
                         $custom_logo_id = get_theme_mod('custom_logo');
                         $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
                     }
                     ?>
-                    <img src="<?php echo $logo[0]; ?>" alt="Logo" />
-            </a>
-            <nav role="navigation" aria-label="Main Menu">
-                <ul id="main-menu">
+                    <img src="<?php echo $logo[0]; ?>" alt="" />
+                </a>
+                <label for="check__toggle" class="btn__toggle_close"><i class="fa-solid fa-xmark"></i></label>
+				<a href="/" class="m-logo">
+                    <?php
+                    if (function_exists('the_custom_logo')) {
+                        $custom_logo_id = get_theme_mod('custom_logo');
+                        $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+                    }
+                    ?>
+                    <img src="<?php echo $logo[0]; ?>" alt="" />
+                </a>
+                <ul class="m-nav__list" id="m-nav__list__desktop">
                     <?php
                     $menu_items = wp_get_nav_menu_items('main_menu');
                     if ($menu_items) {
                         foreach ($menu_items as $menu_item) {
-                            // Check if the current menu item has children
                             $has_children = in_array('menu-item-has-children', $menu_item->classes);
 
-                            // Output menu item with or without sub-menu
-                            echo '<li' . ($has_children ? ' class="menu-item-has-children"' : '') . '>';
-                            echo '<a href="' . esc_url($menu_item->url) . '">' . esc_html($menu_item->title) . '</a>';
+                            echo '<li' . ($has_children ? ' class="menu-item-has-children menu-item"' : ' class="menu-item"') . '>';
+                            echo '<a href="' . esc_url($menu_item->url) . '"><span>' . esc_html($menu_item->title) . '</span></a>';
 
-                            // If the current menu item has children, display them
                             if ($has_children) {
                                 echo '<ul class="sub-menu">';
                                 foreach ($menu_items as $child_menu_item) {
                                     if ($child_menu_item->menu_item_parent == $menu_item->ID) {
-                                        echo '<li><a href="' . esc_url($child_menu_item->url) . '">' . esc_html($child_menu_item->title) . '</a></li>';
+                                        echo '<li class="menu-item"><a href="' . esc_url($child_menu_item->url) . '"><span>' . esc_html($child_menu_item->title) . '</span></a></li>';
                                     }
                                 }
                                 echo '</ul>';
@@ -67,8 +85,34 @@ if (!defined('ABSPATH')) {
                     }
                     ?>
                 </ul>
-            </nav>
+                <div class="m-full-menu-active" style="margin-right: 13px;">
+                    <ul class="m-nav__list" id="m-nav__list__all">
+						<?php
+							$menu_items = wp_get_nav_menu_items('main_menu');
+							if ($menu_items) {
+								foreach ($menu_items as $menu_item) {
+									$has_children = in_array('menu-item-has-children', $menu_item->classes);
 
+									echo '<li' . ($has_children ? ' class="menu-item-has-children menu-item"' : ' class="menu-item"') . '>';
+									echo '<a href="' . esc_url($menu_item->url) . '"><span>' . esc_html($menu_item->title) . '</span></a>';
+
+									if ($has_children) {
+										echo '<ul class="sub-menu">';
+										foreach ($menu_items as $child_menu_item) {
+											if ($child_menu_item->menu_item_parent == $menu_item->ID) {
+												echo '<li class="menu-item"><a href="' . esc_url($child_menu_item->url) . '"><span>' . esc_html($child_menu_item->title) . '</span></a></li>';
+											}
+										}
+										echo '</ul>';
+									}
+
+									echo '</li>';
+								}
+							}
+						?>
+                    </ul>
+				</div>
+            </nav>
         </div>
     </header>
     <div class="gap-menu"></div>
